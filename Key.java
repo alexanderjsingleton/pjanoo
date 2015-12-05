f68013ef3619f8798998156d1b6a7a95aaea7295
Alex Singleton <alexanderjsingleton@gmail.com>
2015-12-05 16:02:39 -0600

    privatized event trigger using boolean values

--- Key.java
+++ Key.java
@@ -14,6 +14,14 @@ public class Key extends Actor
      */
     public void act()
     {
+        if ( Greenfoot.isKeyDown("g") ) {
+            setImage("white-key-down.png");
+            isDown = true;
+        }
+        else{
+            setImage("white-key.png");
+            isDown = false;
+        }
     }
 }
 
